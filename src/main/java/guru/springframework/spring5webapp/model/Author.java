803e0ee97773d4b9ac0c0e96e15509abfdf2e4a7
pdione <dione.pape@gmail.com>
2017-10-11 10:34:25 +0200

    Adding author controller and displaying author list

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -62,6 +62,14 @@ public class Author {
 	this.books = books;
     }
 
+    public Long getId() {
+	return id;
+    }
+
+    public void setId(Long id) {
+	this.id = id;
+    }
+
     @Override
     public int hashCode() {
 	final int prime = 31;
